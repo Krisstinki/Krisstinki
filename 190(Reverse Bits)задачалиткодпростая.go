9a2ed567b162655a2a75c29225b40b86b77b2a58
nnissa <kristinamedvid8@gmail.com>
2025-01-28 01:08:19 +0200

    190(Reverse Bits)задачалиткодпростая.go
    Праивльно решение задачи на голанг,
    функция `reverseBits` инвертирует биты 32-битного числа:
    1. создаем переменную `result`, в которой будем собирать перевернутые биты,
    2. цикл проходит по всем 32 битам:
     - сдвигаем `result` влево, чтобы освободить место для нового бита,
     - извлекаем последний бит из `num` и добавляем его в `result`,
     - сдвигаем `num` вправо, чтобы перейти к следующему биту.
    3. возвращаем результат, где биты числа инвертированы,
    пример: для числа `43261596` (в двоичной форме `00000010100101000001111010011100`) результат будет `964176192` (`00111001011110000010100101000000`).

---
+++ 190(Reverse Bits)задачалиткодпростая.go
@@ -0,0 +1,9 @@
+func reverseBits(num uint32) uint32 {
+    var result uint32
+    for i := 0; i < 32; i++ {
+        result = result << 1
+        result |= num & 1
+        num >>= 1
+    }
+    return result
+}
